3f07a52fa3f29bc52efe8e9fd3fc0987802d9c75
Patrick Harrison <jahman@bellatlantic.net>
2019-07-01 20:45:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,20 @@
-def reverse_each_word ["reverse each word"]
-  ["reverse each word"].each do {|n|"n.reverse"]
-   end
+def how_are_you (how_are_you)
+    temp_array = []
+    how_are_you_array = []
+
+    i = how_are_you.length
+
+temp_array.push(how_are_you.split(""))
+
+temp_array.each do |item|
+
+    until i < 0
+        how_are_you_array.push(item[i])
+        i -= 1
+    end
+    #print reverse_array.inspect
+end
+
+how_are_you_array.join
+
 end
