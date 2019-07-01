57f522458bcae304582be1b153b3859db2a49bc8
Patrick Harrison <jahman@bellatlantic.net>
2019-07-01 21:58:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
-def reverse_each_word (sentence)
-    reverse_each_word_array = sentence.split
-    final_array=[]
-    reverse_each_word_array.each do |word|
-    final_array << word.reverse  
+# def reverse_each_word (sentence)
+#     reverse_each_word_array = sentence.split
+#     final_array=[]
+#     reverse_each_word_array.each do |word|
+#     final_array << word.reverse  
 
- end
- return final_array.join(" ")
-end
+# end
+# return final_array.join(" ")
+# end
