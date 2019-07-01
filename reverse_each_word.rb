5f6b4195e0f2cf6cd8e917ee2badf78a6e2ad016
Patrick Harrison <jahman@bellatlantic.net>
2019-07-01 20:55:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -43,3 +43,25 @@ reverse_each_word_array.join
 end
 
 print reverse_each_word("hi ,how are you today ")
+def reverse_each_word (reverse_each_word)
+    temp_array = []
+    reverse_each_word_array = []
+
+    i = reverse_each_word.length
+
+temp_array.push(reverse_each_word.split(""))
+
+temp_array.collect do |item|
+
+    until i < 0
+        reverse_each_word_array.push(item[i])
+        i -= 1
+    end
+
+end
+
+reverse_each_word_array.join
+
+end
+
+print reverse_each_word("reverse each word ")
